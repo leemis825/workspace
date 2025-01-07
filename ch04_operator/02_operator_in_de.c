fa3edbccdd56a5368140c220a082cc3c8185858b
leemis825 <dlalstj0811@naver.com>
2025-01-07 11:47:49 +0900

    Lecture ch04

---
+++ ch04_operator/02_operator_in_de.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+// 증감 연산자
+// ++ : 1 증가
+// -- : 1 감소
+
+// ++a : 전위형 증감 연산자
+// a++ : 후위형 증감 연산자
+
+int main(){
+    int a =5, b=5;
+    int pre, post;
+    printf("초기값: %d, %d\n",a, b);
+
+    pre = (++a) *2;  // 무조건 1 더하고 실행
+    post = (b++)*2;  // 실행 후 1 더하기
+
+    printf("초기값: %d, %d\n",a, b);
+    printf("전위형 %d\n",pre);
+    printf("후위형 %d\n",post);
+
+    int c = b*2;
+    printf("%d",c);
+
+}
